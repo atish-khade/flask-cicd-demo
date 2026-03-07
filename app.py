3f6efcf1acd038abc714e0b5fcf47893e6dd96da
atish-khade <atishkhade05@gmail.com>
2026-03-07 11:20:57 +0000

    Third commit

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from CI/CD Pipeline!"
+    return "Hello from CI/CD Pipeline! This line is added on 2nd edit."
 
 app.run(host="0.0.0.0", port=5000)
