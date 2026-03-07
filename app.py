8d740525ee409a3bbdb37399be67609f36953479
atish-khade <atishkhade05@gmail.com>
2026-03-07 11:58:11 +0000

    Checking auto triggers

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from CI/CD Pipeline! This line is added on 2nd edit."
+    return "Hello from CI/CD Pipeline! This line is added on 2nd edit. This line added to test git webhook."
 
 app.run(host="0.0.0.0", port=5000)
