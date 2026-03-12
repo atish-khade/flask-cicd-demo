c198a75ddc94659a44366458611236e5ec07cb4a
atish-khade <atishkhade05@gmail.com>
2026-03-12 10:26:23 +0000

    Changed made by developer

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from CI/CD Pipeline! This line is added on 2nd edit. Webhook was tested on 12-Mar-26."
+    return "Hello from CI/CD Pipeline! This line is added on 2nd edit. Webhook was tested on 12-Mar-26. This line was added after"
 
 app.run(host="0.0.0.0", port=5000)
